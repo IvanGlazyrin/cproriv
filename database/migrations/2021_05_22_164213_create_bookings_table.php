c3451888d5f8c203e6bf7ee4776fbdee709f0da7
IvanGlazyrin <glazrus97@gmail.com>
2021-05-22 21:11:56 +0300

    Add migrations

---
+++ database/migrations/2021_05_22_164213_create_bookings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('rental_point_id');
+            $table->date('booking_date');
+            $table->integer('equipment_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
---
+++ database/migrations/2021_05_22_164313_create_marks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMarksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('marks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('desctiption');
+            $table->integer('user_id');
+            $table->integer('latitude');
+            $table->integer('longitude');
+            $table->integer('mark_color_id');
+            $table->integer('mark_status_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('marks');
+    }
+}
